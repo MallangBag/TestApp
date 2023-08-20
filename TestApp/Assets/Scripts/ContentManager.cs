dac4c9e7ef4fc38ff3f699e45ee6dc25b3ef75d3
MallangBag <dlxotkddl1@naver.com>
2023-08-20 17:38:27 +0900

    Update Main

---
+++ TestApp/Assets/Scripts/ContentManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContentManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Content;
+
+    [SerializeField]
+    private GameObject textBox;
+
+    public void ContentSize()
+    {
+        //content의 크기 변경
+        Vector2 pos2 = new();
+        float height = textBox.GetComponent<RectTransform>().sizeDelta.y;
+        pos2.y = height;
+        Content.GetComponent<RectTransform>().sizeDelta = pos2;
+
+        //텍스트 위치 수정
+        pos2 = textBox.transform.localPosition;
+        pos2.y = -((height / 2) + 16);
+        textBox.transform.localPosition = pos2;
+    }
+
+
+}
